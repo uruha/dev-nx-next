89d1916eb559cf89e26cd5544d310e3156da5ad8
kotaro chiba <kota@kota-ro.local>
2021-07-31 11:36:06 +0900

    update: add admin project

---
+++ apps/admin/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+import './styles.css';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to admin!</title>
+      </Head>
+      <div className="app">
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
---
+++ apps/admin/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+export function Index() {
+  return (
+    <div className={styles.page}>
+      admin
+    </div>
+  );
+}
+
+export default Index;

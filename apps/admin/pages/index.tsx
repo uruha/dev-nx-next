a0bd9868758a0f0a09684bdf59f31d5866728072
kotaro chiba <kota@kota-ro.local>
2021-09-09 09:38:31 +0900

    add ISR sample page

--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -1,10 +1,16 @@
 import styles from './index.module.css';
+import Link from 'next/link';
 
 export function Index() {
   return (
-    <div className={styles.page}>
-      admin
-    </div>
+    <>
+      <div className={styles.page}>
+        admin
+      </div>
+      <Link href={'/isr'}>
+        <a>to ISR page</a>
+      </Link>
+    </>
   );
 }
 

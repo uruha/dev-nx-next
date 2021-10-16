031fb257cb1e623cd26a98f7c7aad953426217c9
kotaro chiba <kota@kota-ro.local>
2021-10-16 12:40:24 +0900

    add: useRouter for SSR couterplan provisional processing

--- apps/admin/pages/daily-events/list.tsx
+++ apps/admin/pages/daily-events/list.tsx
@@ -1,11 +1,19 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function List() {
   const router = useRouter();
+  const [dateQuery, setDateQuery] = useState('取得中');
+
+  useEffect(() => {
+    if (router && router.query) {
+      setDateQuery(router.query.date as string);
+    }
+  }, [router]);
 
   return (
     <>
-      <h2>{router.query.date}</h2>
+      <h2>{dateQuery}</h2>
     </>
   );
 }

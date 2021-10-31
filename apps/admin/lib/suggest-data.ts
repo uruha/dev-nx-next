7a91b21e6388fedd69f2649588d89622eb64c246
kotaro chiba <kota@kota-ro.local>
2021-10-31 20:51:20 +0900

    add: suggest form logics sample

---
+++ apps/admin/lib/suggest-data.ts
@@ -0,0 +1,38 @@
+export type FoodAndDrink = {
+  id: number;
+  name: string;
+  unit: string;
+}
+
+export const foodAndDrink: FoodAndDrink[] = [
+  {
+    id: 1,
+    name: 'ビール',
+    unit: 'パイント'
+  },
+  {
+    id: 2,
+    name: 'ピザ',
+    unit: 'piece'
+  },
+  {
+    id: 3,
+    name: 'ごはん',
+    unit: '杯'
+  },
+  {
+    id: 4,
+    name: 'びーるおぶびーる',
+    unit: 'パイント'
+  },
+  {
+    id: 5,
+    name: 'Beer',
+    unit: 'Pint'
+  },
+  {
+    id: 6,
+    name: 'ビール 350mL',
+    unit: 'パイント'
+  },
+];
--- apps/admin/pages/form.tsx
+++ apps/admin/pages/form.tsx
@@ -1,25 +1,119 @@
 import { useForm, Controller, NestedValue, SubmitHandler } from 'react-hook-form';
 import Autosuggest from 'react-autosuggest';
+import { useState } from 'react';
 
-const suggestions = [
-  {
-    id: 1,
-    name: 'Beer',
-    unit: 'pint'
-  }
-];
+import { FoodAndDrink, foodAndDrink } from '../lib/suggest-data';
 
 function SuggestForm() {
   // react-hook-form setting
   const {
     handleSubmit,
-    
     control
   } = useForm();
 
   const onSubmit = data => console.log(data);
 
   // react-autosuggestion setting
+  // prepare suggest hooks
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState<FoodAndDrink[]>([]);
+  
+  // english string manipulation (※ also supports Japanese)
+  const toTriming = (inputString: string) => inputString.trim();
+  const toLowerCased = (inputString: string) => inputString.toLowerCase();
+  const isIncludes = (suggestString: string, inputString: string) => suggestString.includes(inputString);
+
+  // japanese string manipulation
+  /**
+   * @NOTE unicode sample
+   * @see https://qiita.com/graminume/items/2ac8dd9c32277fa9da64
+   */
+  const hiraKanaUnicode = /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/;
+  const isJapanase = (inputString: string) => hiraKanaUnicode.test(inputString);
+
+  const hiraganaUnicode = /[\u3040-\u309f]/;
+  const isHiragana = (inputString: string) => hiraganaUnicode.test(inputString);
+  const hiraToKana = (inputStrig: string) => {
+    return inputStrig.replace(/[\u3040-\u309f]/g, function(match) {
+        const character = match.charCodeAt(0) + 0x60;
+        return String.fromCharCode(character);
+    });
+  }
+
+  const katakanaUnicode = /[\u30a0-\u30ff]/;
+  const isKatakana = (inputString: string) => katakanaUnicode.test(inputString);
+  const kanaToHira = (inputString: string) => {
+    return inputString.replace(/[\u30a1-\u30f6]/g, function(match) {
+        const character = match.charCodeAt(0) - 0x60;
+        return String.fromCharCode(character);
+    });
+  }
+
+  // processed string for suggestion multiple language for english and japanese
+  const createInputProcessedString = (inputValue: string): string => toLowerCased(toTriming(inputValue));
+  const judgeHiraKana = (inputString: string, suggestString: string) => {
+    if(isHiragana(inputString)) {
+      /**
+       * @NOTE
+       * 入力値が「ひらがな」の場合、全ての入力候補を「ひらがな」変換して総当り
+       */
+      return isIncludes(kanaToHira(suggestString), inputString);
+    }
+    if(isKatakana(inputString)){
+      /**
+       * @NOTE
+       * 入力値が「カタカナ」の場合、全ての入力候補を「カタカナ」変換して総当り
+       */
+      return isIncludes(hiraToKana(suggestString), inputString);
+    }
+  }
+
+
+  /**
+   * @NOTE 2021/10/31 現在
+   * suggestion が任意のオブジェクトで key/value の key を代入する形で対応する時（動的に指定する）、
+   * onSuggestionsClearRequested のタイミングで target の取得ができず上手く行かない
+   */
+  // const getSuggestionValue = (suggestion, target?) => {
+  //   if(target && target.length === 0) return suggestions[target];
+  //   if(!target) return suggestion;
+  // }
+
+  // prepare suggest logics
+  const getSuggestionValue = suggestion => suggestion.name;
+  const getSuggestions= <S extends string, T extends Record<string, unknown>>(inputValue: string, candidateList: T[]): T[] => {
+    const inputProcessedString = createInputProcessedString(inputValue);
+    const inputLength = inputProcessedString.length;
+
+    return inputLength === 0
+      ? []
+      : candidateList.filter(candidate => {
+        if (isJapanase(inputProcessedString)) {
+          return judgeHiraKana(inputProcessedString, getSuggestionValue(candidate));
+        } else {
+          return isIncludes(toLowerCased(getSuggestionValue(candidate)), inputProcessedString);
+        }
+      });
+  };
+
+  const handleOnSuggestionSelected = (_, { suggestion }) => {
+    console.log(suggestion);
+  }
+
+  const renderSuggestion = suggestion => (
+    <div>
+      {suggestion.name}
+    </div>
+  );
+
+  const inputProps = {
+    placeholder: 'Type "ビ"',
+    value,
+    onChange: (_, { newValue }) => {
+      setValue(newValue);
+    }
+  };
+
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -28,18 +122,22 @@ function SuggestForm() {
         <Controller
           name="suggest"
           control={control}
-          render={({
-            field: { onChange, onBlur, value, name, ref },
-            fieldState: { invalid, isTouched, isDirty, error },
-            formState
-          }) => (
+          render={() => (
             <Autosuggest
               // required props
               suggestions={suggestions}
-              onSuggestionsFetchRequested={}
-              getSuggestionValue={}
-              renderSuggestion={}
-              inputProps={}
+              onSuggestionsFetchRequested={({ value }) => {
+                setValue(value);
+                setSuggestions(
+                  getSuggestions(value, foodAndDrink)
+                );
+              }}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
+              onSuggestionsClearRequested={() => setSuggestions([])}
+              // options props
+              onSuggestionSelected={handleOnSuggestionSelected}
             />
           )}
         />

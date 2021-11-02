33240d12c225e21de6a59770b0db8f30a20da53c
Chiba Kotaro <kotaro_chiba@ChibanoMacBook-Pro.local>
2021-11-02 11:34:12 +0900

    update: sample data type

--- apps/admin/lib/suggest-data.ts
+++ apps/admin/lib/suggest-data.ts
@@ -1,38 +1,45 @@
 export type FoodAndDrink = {
-  id: number;
+  id?: number;
   name: string;
-  unit: string;
+  dosage?: number | null;
+  unit?: string;
 }
 
 export const foodAndDrink: FoodAndDrink[] = [
   {
     id: 1,
     name: 'ビール',
+    dosage: null,
     unit: 'パイント'
   },
   {
     id: 2,
     name: 'ピザ',
+    dosage: null,
     unit: 'piece'
   },
   {
     id: 3,
     name: 'ごはん',
+    dosage: null,
     unit: '杯'
   },
   {
     id: 4,
     name: 'びーるおぶびーる',
+    dosage: null,
     unit: 'パイント'
   },
   {
     id: 5,
     name: 'Beer',
+    dosage: null,
     unit: 'Pint'
   },
   {
     id: 6,
     name: 'ビール 350mL',
+    dosage: null,
     unit: 'パイント'
   },
 ];

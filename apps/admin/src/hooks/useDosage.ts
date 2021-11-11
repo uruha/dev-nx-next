62ace69bf454517f436f76f219e6824421715fed
Chiba Kotaro <kotaro_chiba@ChibanoMacBook-Pro.local>
2021-11-11 15:32:52 +0900

    delete non used hooks

--- apps/admin/src/hooks/useDosage.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent, useState } from "react";
-
-export const useDosage = (initialDosage = '0') => {
-  const [dosage, setDosage] = useState(initialDosage);
-  const changedDosage = (e: ChangeEvent<HTMLInputElement>) => {
-    setDosage(e.target.value);
-  };
-  return { dosage, changedDosage };
-};
--- apps/admin/src/hooks/useUnit.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeEvent, useState } from "react";
-
-export const useUnit = (initialUnit = '') => {
-  const [unit, setUnit] = useState(initialUnit);
-  const changedUnit = (e: ChangeEvent<HTMLSelectElement>) => {
-    setUnit(e.target.value);
-  };
-  return { unit, changedUnit };
-};

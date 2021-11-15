e6db571c3ba782446b7000bebbc2ff52d18489de
Chiba Kotaro <kotaro_chiba@ChibanoMacBook-Pro.local>
2021-11-15 17:21:44 +0900

    update: change CSR type FullCalendar rendering

--- apps/admin/pages/calendar.tsx
+++ apps/admin/pages/calendar.tsx
@@ -1,101 +1,15 @@
-import Router from 'next/router';
+import dynamic from 'next/dynamic';
 
-import FullCalendar, { DatesSetArg, EventClickArg, EventContentArg, EventSourceInput } from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+const Calendar = dynamic(() => import('../src/components/Calendar'), {
+  ssr: false
+});
 
-const isEmptyObj = (obj) => 
-  Object.keys(obj).length === 0 && obj.constructor === Object;
-
-const events: EventSourceInput = [
-  { start: '2021-10-05', title: '入力あり' },
-  { start: '2021-10-07', title: '入力あり' },
-  { start: '2021-10-13', title: '入力あり' },
-  {
-    start: '2021-10-18',
-    title: '入力あり',
-    extendedProps: {
-      data: 'hoge'
-    }
-  }
-];
-
-const dateNow = new Date();
-const twoMonthsAfterTheCurrentMonth =
-  `${dateNow.getFullYear()}-${dateNow.getMonth()+3}-01`;
-
-const handleDateClick = (info: DateClickArg) => {
-  console.log(info);
-  Router.push({
-    pathname: '/daily-events/list',
-    query: {
-      date: info.dateStr
-    }
-  });
-};
-
-const handleChangeDate = (dateInfo: DatesSetArg) => {
-  console.log(dateInfo);
-  console.log(`current Year: ${dateInfo.view.currentStart.getFullYear()}`);
-  console.log(`current month: ${dateInfo.view.currentStart.getMonth()+1}`);
-};
-
-const handleClickEvent = (clickEventInfo: EventClickArg) => {
-  console.log(clickEventInfo);
-  console.log(clickEventInfo.event.startStr);
-  Router.push({
-    pathname: '/daily-events/list',
-    query: {
-      date: clickEventInfo.event.startStr
-    }
-  });
-};
-
-const customEventContent = (eventInfo: EventContentArg) => {
-  console.log(eventInfo);
+const SampleCalendar = () => {
   return (
     <>
-      <span>{eventInfo.event.title}</span>
-      {!isEmptyObj(eventInfo.event.extendedProps)
-        && <p>{eventInfo.event.extendedProps.data}</p>}
+      <Calendar />
     </>
   );
-}
-
-export default function Index() {
-  return <FullCalendar
-    // eslint-disable-next-line
-    // @ts-ignore
-    plugins={[
-      dayGridPlugin,
-      interactionPlugin
-    ]}
-    locale="ja"
-    /**
-     * @see https://fullcalendar.io/docs/initialEvents
-     * initialEvents だとカレンダー上の操作などで情報が更新されないので events に寄せておいたほうが良さそう
-     */
-    events={events}
-    initialView='dayGridMonth'
-    validRange={{
-      end: `${twoMonthsAfterTheCurrentMonth}`
-    }}
-    headerToolbar={{
-      left: 'prev',
-      center: 'title',
-      right: 'next'
-    }}
-    /**
-     * @NOTE
-     * スクロールバー対策だと絶対値で割り切ったほうが良さそう
-     */
-    height={600}
-    dateClick={handleDateClick}
-    datesSet={handleChangeDate}
-    /**
-     * custom date cell
-     */
-    eventContent={customEventContent}
-    eventClick={handleClickEvent}
-  />
 };
+
+export default SampleCalendar;

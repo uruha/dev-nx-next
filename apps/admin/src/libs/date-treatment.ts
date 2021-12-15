5e1937a40384835527d32e9902c9e05876597aad
Chiba Kotaro <kotaro_chiba@ChibanoMacBook-Pro.local>
2021-12-15 14:55:32 +0900

    update: date category

--- apps/admin/src/libs/date-treatment.ts
+++ apps/admin/src/libs/date-treatment.ts
@@ -29,21 +29,53 @@ const filterHasSunday = getInTheLastSixDays().filter(
 
 const firstSunday = filterHasSunday[0];
 
-const getThirteenWeeksWorthOfDays = (length = 14): Dayjs[] => {
-  const lastThreeMonthsSunday = [];
+const getInTheFutureSixDaysFromFirstSunday = (length = 6) => {
+  const futureSixDays = [];
 
   for (let i = 1; i <= length; i++) {
-    const addDay = firstSunday.add(i, "w");
-    lastThreeMonthsSunday.push(addDay);
+    const addDay = firstSunday.add(i, "d");
+    futureSixDays.push(addDay);
   }
 
-  return lastThreeMonthsSunday;
+  return futureSixDays;
 };
 
-const lastSaturdayOfTheLastThreeMonths = getThirteenWeeksWorthOfDays()[13].subtract(
-  1,
-  "s"
+const filterHasSaturday = getInTheFutureSixDaysFromFirstSunday().filter(
+  (d) => d.format("ddd") === "Sat"
 );
+
+const firstSaturday = filterHasSaturday[0];
+
+// 日曜日版
+// const getThirteenWeeksWorthOfDays = (length = 14): Dayjs[] => {
+//   const lastThreeMonthsSunday = [];
+
+//   for (let i = 1; i <= length; i++) {
+//     const addDay = firstSunday.add(i, "w");
+//     lastThreeMonthsSunday.push(addDay);
+//   }
+
+//   return lastThreeMonthsSunday;
+// };
+
+// const lastSaturdayOfTheLastThreeMonths = getThirteenWeeksWorthOfDays()[13].subtract(
+//   1,
+//   "s"
+// );
+
+// 土曜日版
+const getThirteenWeeksWorthOfDays = (length = 13): Dayjs[] => {
+  const lastThreeMonthsSaturday = [];
+
+  for (let i = 0; i <= length; i++) {
+    const addDay = firstSaturday.add(i, "w");
+    lastThreeMonthsSaturday.push(addDay);
+  }
+
+  return lastThreeMonthsSaturday;
+};
+
+const lastSaturdayOfTheLastThreeMonths = getThirteenWeeksWorthOfDays()[13].endOf('hour');
 
 const thirteenWeeksWorthOfDays = getThirteenWeeksWorthOfDays();
 thirteenWeeksWorthOfDays.splice(13, 13, lastSaturdayOfTheLastThreeMonths);
